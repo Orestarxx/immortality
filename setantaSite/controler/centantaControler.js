5b8d08a74622bcfc73aa2829014f848ad1da2c41
Orestarxx <oresto322@gmail.com>
2023-03-28 00:42:14 +0300

    in progress

--- setantaSite/controler/centantaControler.js
+++ setantaSite/controler/centantaControler.js
@@ -1,13 +1,22 @@
+
 let content = document.getElementById('contentHolder');
+
+//інформація про юзера
 let userIcon = document.getElementById('userInformation');
+let wall = document.createElement("div");
+wall.setAttribute('id','wall');
+content.append(wall)
 let userDiv = document.createElement('div');
 userDiv.setAttribute('id','userDiv');
-content.append(userDiv);
+wall.append(userDiv);
 
 userIcon.addEventListener('click',()=>{
-    if (userDiv.style.display === "block") {
-                userDiv.style.display = "none";
+    if (wall.style.display === "block") {
+                wall.style.display = "none";
         } else {
-                userDiv.style.display = "block";
+                wall.style.display = "block";
         }
 })
+
+let search = document.getElementById('search');
+
