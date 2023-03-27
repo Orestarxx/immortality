7376dae3398d1a559ba62735f66cbcf4adae8558
Orestarxx <oresto322@gmail.com>
2023-03-27 20:14:12 +0300

    in progress

--- setantaSite/controler/centantaControler.js
+++ setantaSite/controler/centantaControler.js
@@ -1,1 +1,13 @@
+let content = document.getElementById('contentHolder');
+let userIcon = document.getElementById('userInformation');
+let userDiv = document.createElement('div');
+userDiv.setAttribute('id','userDiv');
+content.append(userDiv);
 
+userIcon.addEventListener('click',()=>{
+    if (userDiv.style.display === "block") {
+                userDiv.style.display = "none";
+        } else {
+                userDiv.style.display = "block";
+        }
+})
