63e25d981e87fd3e31a8d7abd6b66d207da5fdfb
Orestarxx <oresto322@gmail.com>
2023-03-28 18:05:42 +0300

    in progress

--- setantaSite/controler/centantaControler.js
+++ setantaSite/controler/centantaControler.js
@@ -5,6 +5,7 @@ let content = document.getElementById('contentHolder');
 let userIcon = document.getElementById('userInformation');
 let wall = document.createElement("div");
 wall.setAttribute('id','wall');
+wall.style.display = 'none'
 content.append(wall)
 let userDiv = document.createElement('div');
 userDiv.setAttribute('id','userDiv');
--- setantaSite/premierLeague/premierLeague.js
+++ setantaSite/premierLeague/premierLeague.js
@@ -1448,26 +1448,65 @@ let data = {
 };
 let pLeagueInfo = data.response;
 
-// let premierH3 = document.getElementById('premier');
-//  let contentHolder = document.getElementById('contentHolder');
-//  let premierContent = document.createElement('div');
-//  premierContent.setAttribute('id','premierLeague');
-//  contentHolder.append(premierContent);
+let sliderContainer = document.querySelector('.slider-container');
+let innerSlider = document.querySelector('.inner-slider');
 
-// premierH3.addEventListener('click',(eo) =>{
-//         eo.preventDefault()
-//         if (premierContent.style.display === "block") {
-//                 premierContent.style.display = "none";
-//                 premierH3.style.color = "black"
-//         } else {
-//                 premierH3.style.color = "orange"
-//                 premierContent.style.display = "block";
-//         }
-// })
+for (const result of pLeagueInfo) {
+        console.log(result);
+        let card = document.createElement('div');
+card.classList.add('card');
+innerSlider.append(card)
+
+}
 
 
 
 
+let pressed = false;
+let startX;
+let x;
+
+
+sliderContainer.addEventListener("mousedown", (e) => {
+        pressed = true;
+        startX = e.offsetX - innerSlider.offsetLeft;
+        sliderContainer.style.cursor = "grabbing";
+});
+
+sliderContainer.addEventListener("mouseenter", () => {
+        sliderContainer.style.cursor = "grab";
+});
+
+sliderContainer.addEventListener("mouseup", () => {
+        sliderContainer.style.cursor = "grab";
+        pressed = false;
+});
+
+sliderContainer.addEventListener("mousemove", (e) => {
+        if (!pressed) return;
+        e.preventDefault();
+
+        x = e.offsetX;
+
+        innerSlider.style.left = `${x - startX}px`;
+});
+
+const checkBoundary = () => {
+        let outer = sliderContainer.getBoundingClientRect();
+        let inner = innerSlider.getBoundingClientRect();
+
+        if (parseInt(innerSlider.style.left) > 0) {
+                innerSlider.style.left = "0px";
+        }
+
+        if (inner.right < outer.right) {
+                innerSlider.style.left = `-${inner.width - outer.width}px`;
+        }
+};
+
+sliderContainer.addEventListener("mousemove", (e) => {
+        checkBoundary();
+});
 
 
 
